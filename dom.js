27ca4c51e50a5b7922650f1c5d94364735ef6f7b
Hariharan1714 <hariharan10420>
2023-05-21 17:17:55 +0530

    fontColor

--- dom.js
+++ dom.js
@@ -1,3 +1,11 @@
 var Header = document.getElementById('header-title');
 console.log(Header);
 Header.style.borderBottom = 'solid 3px #000';
+
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items [1].textContent = 'Hello 2';
+items [1].style.fontweight = 'bold';
+items [1].style.color = 'green';
